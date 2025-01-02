489f3fdc16d54ff58530117ab8b923aaa3105a72
ganeshrahul <ganeshrahul@nithra.mobi>
2025-01-03 02:28:35 +0530

    Optimizations

--- models/Service.js
+++ models/Service.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ServiceSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
     description: { type: String, trim: true },
-    createdAt: { type: Date, default: Date.now },
+    // createdAt: { type: Date, default: Date.now },
 });
 
 // Adding indexes
